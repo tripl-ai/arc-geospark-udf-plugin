d4436fdc422e9bf6f250280f8ccc1bdd047c0e02
John Bruce <john.a.m.bruce@gmail.com>
2023-03-26 23:11:01 -0400

    Release 1.3.0 (#2)
    * Update to Arc 3.13.2 and Sedona 1.4.0 (#1)
    ---------
    
    Co-authored-by: Mike Seddon <seddonm1@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Change Log
 
+## 1.3.0
+
+- bump Arc to 3.13.2.
+- updated to [org.apache.sedona](https://sedona.apache.org/) 1.4.0.
+
 ## 1.2.0
 
 - change to [org.apache.sedona](https://sedona.apache.org/) depedencies.
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object Dependencies {
   // versions
-  lazy val sparkVersion = "3.0.2"
+  lazy val sparkVersion = "3.3.2"
 
   // testing
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "3.7.0" % "provided"
+  val arc = "ai.tripl" %% "arc" % "3.13.2" % "provided"
 
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
 
-  val geospark = "org.apache.sedona" %% "sedona-core-3.0" % "1.0.0-incubating"
-  val geosparkSql = "org.apache.sedona" %% "sedona-sql-3.0" % "1.0.0-incubating"
+  val geospark = "org.apache.sedona" %% "sedona-core-3.0" % "1.4.0"
+  val geosparkSql = "org.apache.sedona" %% "sedona-sql-3.0" % "1.4.0"
   val locationtech = "org.locationtech.jts" % "jts-core" % "1.18.0"
   val geotools = "org.datasyslab" % "geotools-wrapper" % "geotools-24.0"
 
